04a64e5628c1d3ee232fba9e2efc61c6019c00e3
Gagan <gaganbrgirish26@gmail.com>
2025-10-07 18:07:06 +0530

    changed the folder structure

--- TimerTrigger/index.js
+++ TimerTrigger/index.js
@@ -1,4 +1,4 @@
 module.exports = async function (context, myTimer) {
     const timeStamp = new Date().toISOString();
-    context.log('Timer function ran!', timeStamp);
+    context.log(`⏰ Timer function executed at: ${timeStamp}`);
 };
